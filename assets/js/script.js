8b0c45059788def71ab8b5d4765720ceee9033d9
Helena Lissenko <helena.lissenko@gmail.com>
2015-08-25 10:58:49 +0300

    mobile view header started

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+var $window = $(window),
+    $ul = $('ul.nav-tabs');
+
+$(window).on('resize', function () {
+    if ($window.width() < 768) {
+        $ul.addClass('nav-stacked');
+    }else{
+
+        $ul.removeClass('nav-stacked')};
+});
